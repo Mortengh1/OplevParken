e3e72b42798374566e329aca22387130f56c8301
Morten Hansen <mortengh@live.dk>
2020-12-10 13:32:45 +0100

    Forside done

--- Booking/js/js.js
+++ Booking/js/js.js
@@ -1,61 +1,84 @@
-const form = document.getElementById('form');
-    const navn = document.getElementById('navn');
-    const email = document.getElementById('email');
-    const telefon = document.getElementById('telefon');
-    const dato = document.getElementById('dato');
+const form = document.getElementById("form");
+const navn = document.getElementById("navn");
+const email = document.getElementById("email");
+const telefon = document.getElementById("telefon");
+const dato = document.getElementById("dato");
 
-    form.addEventListener('submit', e => {
-        e.preventDefault();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-        checkInputs();
-    });
+  checkInputs();
+});
 
-    function checkInputs() {
-        const navnValue = navn.value.trim();
-        const emailValue = email.value.trim();
-        const telefonValue = telefon.value.trim();
-        const datoValue = dato.value.trim();
+function checkInputs() {
+  const navnValue = navn.value.trim();
+  const emailValue = email.value.trim();
+  const telefonValue = telefon.value.trim();
+  const datoValue = dato.value.trim();
 
-        if (navnValue === '') {
-            setErrorFor(navn, 'Navn må ikke være tomt');
-        } else {
-            setSuccessFor(navn);
-        }
+  if (navnValue === "") {
+    setErrorFor(navn, "Navn må ikke være tomt");
+  } else {
+    setSuccessFor(navn);
+  }
 
-        if (emailValue === '') {
-            setErrorFor(email, 'Email må ikke være tomt');
-        } else if (!isEmail(emailValue)) {
-            setErrorFor(email, 'Indtast en korrekt mail');
-        } else {
-            setSuccessFor(email);
-        }
+  if (emailValue === "") {
+    setErrorFor(email, "Email må ikke være tomt");
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, "Indtast en korrekt mail");
+  } else {
+    setSuccessFor(email);
+  }
 
-        if (telefonValue === '') {
-            setErrorFor(telefon, 'Telefon nr. må ikke være tomt');
-        } else {
-            setSuccessFor(telefon);
-        }
+  if (telefonValue === "") {
+    setErrorFor(telefon, "Telefon nr. må ikke være tomt");
+  } else {
+    setSuccessFor(telefon);
+  }
 
-        if (datoValue === '') {
-            setErrorFor(dato, 'Dato må ikke være tomt');
-        } else {
-            setSuccessFor(dato);
-        }
-    }
+  if (datoValue === "") {
+    setErrorFor(dato, "Dato må ikke være tomt");
+  } else {
+    setSuccessFor(dato);
+  }
+}
 
-    function setErrorFor(input, message) {
-        const formControl = input.parentElement;
-        const small = formControl.querySelector('small');
-        formControl.className = 'form-control error';
-        small.innerText = message;
-    }
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+  formControl.className = "form-control error";
+  small.innerText = message;
+}
 
-    function setSuccessFor(input) {
-        const formControl = input.parentElement;
-        formControl.className = 'form-control success';
-    }
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
 
-    function isEmail(email) {
-        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-    }
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
 
+/* Denne function får priser til at fade ind når man scroller ned til 300px */
+var $priser = $(".priserTekst");
+
+$(window).on("scroll", function () {
+  if ($(this).scrollTop() > 300) {
+    $priser.addClass("active");
+  } else {
+    $priser.removeClass("active");
+  }
+});
+
+/* Denne function får brtaling til at fade ind når man scroller ned til 500px */
+var $betaling = $(".betaling");
+
+$(window).on("scroll", function () {
+  if ($(this).scrollTop() > 500) {
+    $betaling.addClass("active");
+  } else {
+    $betaling.removeClass("active");
+  }
+});
